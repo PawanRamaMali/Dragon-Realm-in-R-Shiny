91246feafc272b38c33e45191b9146bf079989ef
Pawan Rama Mali <prm@outlook.in>
2022-03-24 23:54:19 +0530

    version : 1.1   features:     - Added base UI

--- app/app.R
+++ app/app.R
@@ -1,6 +1,38 @@
 library(shiny)
 
 ui <- fluidPage(
+  h1("Intro"),
+  
+  p(
+    "You are in a land full of dragons. In front of you, you see two
+  caves. In onve cave, the dragon is friendly and will share treasure with you.
+  The other dragon is greedy and hungry, and will eat you on sight."
+  ),
+  
+  actionButton(
+    inputId = "intro_continue",
+    label = "Continue"
+  ),
+  
+  h1("Question"),
+  p("Which cave will you go into ? ( 1 or 2)"),
+  
+  actionButton(inputId = "cave_01",
+               label = "Cave 1"),
+  
+  actionButton(inputId = "cave_02",
+               label = "Cave 2"),
+  
+  h1("Consequences"),
+  p("Win text, lose text"),
+  
+  h1("Play again ? "),
+  
+  actionButton(inputId = "play_again",
+               label = "Play again !"),
+  
+  actionButton(inputId = "end_game",
+               label = "No, thank you")
   
 )
 
@@ -12,13 +44,13 @@ shinyApp(ui, server)
 
 #' Flowchart
 #' Start
-#' Introductory text 
+#' Introductory text
 #' Player chooses a cave
 #' Check for a hungry dragon or a friendly dragon
 #'  - player wins
 #'  - player loses
-#' Ask to play again 
+#' Ask to play again
 #'  - if yes, go to start
 #'  - else :
-#'    -- End 
+#'    -- End
 #' End 
